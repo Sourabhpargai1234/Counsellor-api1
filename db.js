5db88fab647cd658322d654f87add350f8bd4958
Sourabh <Sourabhpar@gmail.com>
2024-06-18 14:45:32 +0530

    added everything

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
-import mongoose from 'mongoose'
-import 'dotenv/config'
-var dbHOST= process.env.MongoDBURI
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
+const connectDB = async () => {
+  try {
+    const dbHOST = process.env.MONGODBURI;
+    if (!dbHOST) {
+      throw new Error('DBHOST is not defined in environment variables');
+    }
+    await mongoose.connect(dbHOST);
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error('Error while Mongo Connecting..', err);
+    process.exit(1); // Exit the process with failure
+  }
+};
 
-const connectDB = async() => {
-    try{
-        const connectionInstance = await mongoose.connect(dbHOST)
-        console.log(`Connected to: ${connectionInstance.connection.host}`)
-    }
-    catch(error){
-        console.log("Error occured: ",error)
-        process.exit(1)
-    }
-}
-export default connectDB
+export default connectDB;
