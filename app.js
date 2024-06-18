a418d7ea64d30180baf436abc0417d71497eed1d
Sourabh <Sourabhpar@gmail.com>
2024-06-18 13:38:58 +0530

    added everything

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from "cookie-parser"
+import "dotenv/config"
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["POST","GET","PUT","PATCH"],
+    credentials: true
+}));
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+app.get("/",(req, res) => {
+    res.send("Server running successfully")
+});
+//declaration
+app.use("/api/v1/users", userRouter);
+
+export { app }
--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-require('./db');
-const PORT = process.env.PORT || 8080;
-const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes');
-app.use(express.json());
+import { app } from './app.js'
+import connectDB from './Database/db.js'
+import 'dotenv/config'
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('products api running new deploy');
-});
-app.get('/ping', (req, res) => {
-    res.send('<=PONG=>');
-});
-app.get('/country', (req, res) => {
-    res.send('<=INDIA=>');
-});
-
-app.get('/ping', (req, res) => {
-    res.send('PONG')
-});
-// /products
-app.use('/products', productRoutes);
-// /users
-app.use('/users', userRoutes);
-
-app.listen(8080, () => {
-    console.log('Server is listenin on PORT :' + PORT);
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Caught error: ",error);
+        throw error
+    })
+    app.listen(PORT, ()=> {
+        console.log(`Running on ${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`Error while running server on port: ${PORT}`)
 })
