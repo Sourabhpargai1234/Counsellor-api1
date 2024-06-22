9c78a8d9480e2712286066af3678603a0c4c4101
Sourabh <Sourabhpar@gmail.com>
2024-06-22 14:04:22 +0530

    updated

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 
-/*const storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./public/temp")
     },
@@ -10,8 +10,8 @@ import multer from "multer";
   }) 
 export const upload = multer({ 
     storage,
-})*/
+})
 
-const upload = multer({ dest: '/public/temp/' });
+//const upload = multer({ dest: '/public/temp/' });
 
 export default upload;
