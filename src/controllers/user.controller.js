2f4bef1e37d151ffd7927f80d128713f727945fa
Sourabh <Sourabhpar@gmail.com>
2024-06-23 16:54:20 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-//import { spawn } from 'child_process';
+import { spawn } from 'child_process';
 import path from 'path';
 import 'dotenv/config';
 
